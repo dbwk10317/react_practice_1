11d95f4b75f4e0c43e7ea8620d95609666cba4fd
youhyun <dbwk10317@gmail.com>
2024-05-27 21:37:52 +0900

    add dummy data

---
+++ data/image.ts
@@ -0,0 +1,15 @@
+import * as Util from './util';
+
+export const picsumUrl = (width: number, height: number): string =>
+  `https://picksum.photos/${width}/${height}`;
+
+export const randomImage = (
+  w: number = 1000,
+  h: number = 800,
+  delta: number = 200
+): string => picsumUrl(Util.random(w, w + delta), Util.random(h, h + delta));
+
+export const randomAvatar = () => {
+  const size = Util.random(200, 400);
+  return picsumUrl(size, size);
+};
