8d40d573871a8cfaabe0950657b72d60aa35fc8b
youhyun <dbwk10317@gmail.com>
2024-05-27 20:03:24 +0900

    add formatter and linter

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+
+const settings = [
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+];
+
+export default settings;
